c691ee6e1ab0829f90ec60164b3b60b5e842afa1
billie <billiejocavallaro@gmail.com>
2025-11-21 13:35:46 -0600

    Update Vite config for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pixel-and-noise/',
+  // base: '/pixel-and-noise/', // Only needed for GitHub Pages
 });
 
