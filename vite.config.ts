e13ddbfcd98c50388101745af435d660135f7219
billie <billiejocavallaro@gmail.com>
2025-11-22 11:14:09 -0600

    Update Vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/pixel-and-noise/', // Only needed for GitHub Pages
+  base: '/pixel-and-noise/',
 });
 
